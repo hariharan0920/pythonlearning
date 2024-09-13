abc425e615eac195c56ee401d91f674119c29efc
hariharan0920 <hariharanr0920@gmail.com>
2024-09-13 23:59:18 +0530

    whileloop

--- loop.py
+++ loop.py
@@ -1,5 +1,5 @@
 #general loop statement
-
+# for loop use when the no of iteration is known
 for i in "apple":
     print(i)
 
@@ -58,6 +58,7 @@ for i in range  (1,6):
     sum=sum+i
 print(sum)
 
+# using list in for loop
 # q8 program to read 10 num from keyboard and find their sum and average
 # in print("even",i) can be printed by "," but in input ("even"+str(i)) should be used
 
@@ -94,4 +95,68 @@ for i in range(1,num+1):
     a=i*i*i
     print("num is:",i ,"cube of it is",a)
 
-# q11 
+# q11 print* in rightangled triangle
+
+n=int(input("enter a num"))
+a=[]
+for i in range(1,n):
+    i="*"
+    a.append(i)
+    print(a)
+
+# nested loop
+#genernal nested Loop
+for i in range(1,6):
+    for j in range(1,3):
+        print(j,"apple")
+
+
+#
+
+for i in range(1,8):
+    print("week:",i)
+    for j in range(1,4):
+        print("  day:",j)
+    print("  ... .. ....")
+
+# print num in rightangled triangle
+
+for i in range(1,5):
+    print()
+    for j in range(1,i+1):
+        print("*",end="")
+
+#while loop
+#while use when the no of iterations known
+i=0
+while(i==0):
+    print(i)
+    i=i+1
+
+#print num from1-5 using whileloop
+
+i=1
+while(i<6):
+    print(i)
+    i=i+1
+
+# print series 10,20,...200
+i=10
+while(i<201):
+    print(i)
+    i=i+10
+
+# print first 10 natural num in reverse
+i=10
+while(i>0):
+    print(i,end="")
+    i=i-1
+
+# factorial of a num
+
+i=int(input("enter a num"))
+fact=1
+while(i>0):
+    fact=fact*i
+    print(fact)
+    i=i-1
--- text.py
+++ text.py
@@ -1,9 +1,2 @@
-n=int(input("enter a num"))
-a=[]
-for i in range(1,n):
-    i="*"
-    a.append(i)
-    print(a)
-
 
 
